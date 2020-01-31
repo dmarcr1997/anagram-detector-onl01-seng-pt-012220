929e73372405bb67d363c953cef948e528fedc74
Damon R <dmarcr1997@gmail.com>
2020-01-31 03:38:04 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -14,11 +14,11 @@ class Anagram
       count = 0
       test_ar = word.split('')
       anagram_ar = @word.split('')
-      sorted_test =test_ar.sort!
+      sorted_test = test_ar.sort!
       sorted_anagram = anagram_ar.sort!
       sorted_test.each do |letter|
-        if letter == anagram_ar[count]
-          if count == anagram_ar.length + 1
+        if letter == sorted_anagram[count]
+          if count == sorted_anagram.length + 1
             is_angram = false
           else
             is_angram = true
